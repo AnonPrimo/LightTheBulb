12bebcccbc8d8e922e37672d9fd8acc87416bdf5
jul-ia <julie78sd@gmail.com>
2018-10-24 13:35:07 +0300

    add Energy

---
+++ LightTheBulb_Game/WpfVersion/Energy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfVersion
+{
+    class Energy
+    {
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        public Ellipse Shape{ get; set; }
+
+        ImageBrush image;
+
+        public Energy()
+        {
+
+        }
+
+        public void Move()
+        {
+            //todo
+        }
+        bool MiniGameCheck()
+        {
+            //todo
+            return false;
+        }
+    }
+}
