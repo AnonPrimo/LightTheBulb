d344c2738bf7722b7f329898254495c684351313
One <One@PC_4_7>
2018-11-24 19:45:40 +0200

    minigame controller add

--- LightTheBulb_Game/WpfApp_v2/Controller.cs
+++ LightTheBulb_Game/WpfApp_v2/Controller.cs
@@ -9,56 +9,54 @@ namespace WpfApp_v2
 {
     class Controller
     {
+       // List<PathPlayer> pathPlayer;
 
-        List<PathPlayer> pathPlayer;
+       // public int[,] pole = {
+       // {0,0,3,0,0,0,0,0,0,0},
+       // {0,0,1,0,0,0,0,0,0,0},
+       // {0,0,1,1,0,0,0,0,0,0},
+       // {0,0,0,1,1,0,0,0,0,0},
+       // {0,0,0,0,1,0,0,0,0,0},
+       // {0,0,0,0,1,0,0,0,0,0},
+       // {0,0,0,0,1,1,0,0,0,0},
+       // {0,0,0,0,0,1,1,1,0,0},
+       // {0,0,0,0,0,0,0,1,0,0},
+       // {0,0,0,0,0,0,0,1,1,1}
+       // };
+       // public Controller()
+       // {
+       //     pathPlayer = new List<PathPlayer>();
+       //     for (int i = 0; i < pole.GetLength(0); i++)
+       //     {
+       //         for (int j = 0; j < pole.GetLength(1); j++)
+       //         {
+       //             if (pole[i, j] != 0)
+       //             {
+       //                 pathPlayer.Add(new PathPlayer(i, j, pole[i, j]));
+       //             }
+       //         }
+       //     }
+       // }
+       //public bool Move()
+       // {
 
-        public int[,] pole = {
-        {0,0,3,0,0,0,0,0,0,0},
-        {0,0,1,0,0,0,0,0,0,0},
-        {0,0,1,1,0,0,0,0,0,0},
-        {0,0,0,1,1,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,0,0},
-        {0,0,0,0,1,1,0,0,0,0},
-        {0,0,0,0,0,1,1,1,0,0},
-        {0,0,0,0,0,0,0,1,0,0},
-        {0,0,0,0,0,0,0,1,1,1}
-        };
+       //     foreach (var item in pathPlayer)
+       //     {
+       //         if (item.Value == 3)
+       //         {
+       //             pole[item.I, item.J] = 2;
+       //             continue;
+       //         }
+       //         if (item.Value == 1)
+       //         {
+       //             pole[item.I, item.J] = 3;
+       //             return true; 
+       //         }
+       //     }
+       //     return false;
+       // }
 
 
-        public Controller()
-        {
-            pathPlayer = new List<PathPlayer>();
-            for (int i = 0; i < pole.GetLength(0); i++)
-            {
-                for (int j = 0; j < pole.GetLength(1); j++)
-                {
-                    if (pole[i, j] != 0)
-                    {
-                        pathPlayer.Add(new PathPlayer(i, j, pole[i, j]));
-                    }
-                }
-            }
-        }
 
-
-        public bool Move()
-        {
-
-            foreach (var item in pathPlayer)
-            {
-                if (item.Value == 3)
-                {
-                    pole[item.I, item.J] = 2;
-                    continue;
-                }
-                if (item.Value == 1)
-                {
-                    pole[item.I, item.J] = 3;
-                    return true; 
-                }
-            }
-            return false;
-        }
     }
 }
---
+++ LightTheBulb_Game/WpfApp_v2/MiniGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_v2
+{
+    public class MiniGame
+    {
+        BitmapImage image;
+        Rectangle[,] field; 
+
+        public MiniGame(BitmapImage im)
+        {
+            image = im;
+        }
+
+        private void imageCut()
+        {
+            int w = 4, h = 4;
+            field = new Rectangle[w, h];
+            for (int i = 0; i < w; i++)
+            {
+                for(int j = 0; j < h; j++)
+                {
+
+                }
+            }
+        }
+
+    }
+}
